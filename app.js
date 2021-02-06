72e3bffd6fd24901939213deb693a2aba7fc958c
unknown <tejasborkar74@gmail.com>
2021-02-06 14:25:50 +0530

    Add gitignore and Procfile

--- app.js
+++ app.js
@@ -177,7 +177,12 @@ app.get("/about",function(req,res)
     res.render("about");
 });
 
-app.listen(3000,function()
+let port = process.env.PORT
+if(port == null || port == ""){
+  port = 3000
+}
+
+app.listen(port,function()
 {
     console.log("Listening to server port 3000");
 });
